06816ec1955e095b1263aeb4ab6da93a6033e39c
jamescross <crossjames813@gmail.com>
2021-08-30 16:08:53 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string){
+    let first = string[0].toUpperCase()
+    let rest = string.slice(1)
+    return first + rest
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+
+  static titleize(string){
+    let words = string.split(" ")
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    
+    return this.capitalize(words.map(word =>{
+      
+      if(exceptions.includes(word)){
+        return word
+      }else{
+        return this.capitalize(word)
+      } 
+    }).join(" "))
+  }
 }
